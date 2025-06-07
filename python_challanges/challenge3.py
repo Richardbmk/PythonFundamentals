40e6a4cff6ec82e17ae9ed31becaf2d9b79b47cc
Richardbmk <rdobmk@gmail.com>
2025-06-07 12:17:58 +0200

    Add Simple tasks

---
+++ python_challanges/challenge3.py
@@ -0,0 +1,19 @@
+"""
+Exercise 7
+Below is an approximation of pi:
+
+pi = 3.1415926535
+
+Using f-string formatting, print the approximation of pi to two decimal places as shown below.
+
+Expected result:
+
+Pi: 3.14
+"""
+
+# My solution
+pi = round(3.1415926535, 2)
+print(f"Pi: {pi}")
+
+# Others solution1
+print(f"Pi: {pi:.2f}")
---
+++ python_challanges/challenge4.py
@@ -0,0 +1,10 @@
+"""
+Exercise 10
+Using the print() function and the sep argument set to '#' print the following text:
+
+'summer#time#holiday'
+Theory: https://realpython.com/python-print/#separating-multiple-arguments
+"""
+
+# My solution
+print('summer', 'time', 'holiday', sep='#')
